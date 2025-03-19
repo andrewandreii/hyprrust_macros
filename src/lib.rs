84d49853a8d203861d06d05d532f808c3838bb48
andrewandreii <andrei3f@yahoo.com>
2025-03-19 10:00:28 +0200

    feat: argument type for HyprlandWithData

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,83 @@
 use core::panic;
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::{format_ident, quote};
-use syn::{Attribute, DeriveInput, Expr, ExprLit, Lit, Meta};
-use syn::{ItemEnum, parse_macro_input};
+use syn::punctuated::Punctuated;
+use syn::{parse_macro_input, ExprAssign, Ident, ItemEnum};
+use syn::{Attribute, DeriveInput, Expr, ExprLit, ExprPath, Lit, Meta, Path, Token};
 
-fn parse_command(derive: &DeriveInput) -> Option<String> {
-    let mut attr: Option<&Attribute> = None;
-    for a in &derive.attrs {
-        if let Meta::NameValue(name) = &a.meta {
-            if name.path.is_ident("command") {
-                attr = Some(a);
-            }
-        }
+fn parse_expr_to_ident(expr: &Expr) -> &syn::Ident {
+    match expr {
+        Expr::Path(ExprPath {
+            attrs: _,
+            qself: _,
+            path: Path {
+                leading_colon: _,
+                segments,
+            },
+        }) => &segments.last().unwrap().ident,
+        _ => panic!("Expected identifier"),
+    }
+}
+
+fn parse_command(attr: &Attribute) -> (Option<String>, Option<Ident>) {
+    if !attr.path().is_ident("command") {
+        return (None, None);
     }
 
-    if let Some(attr) = attr {
-        if let Meta::NameValue(name) = &attr.meta {
+    let mut prefix: Option<String> = None;
+    let mut argument_type: Option<Ident> = None;
+
+    match &attr.meta {
+        Meta::NameValue(name) => {
             if let Expr::Lit(ExprLit {
                 attrs: _,
                 lit: Lit::Str(lit_str),
             }) = &name.value
             {
-                return Some(lit_str.value());
+                prefix = Some(lit_str.value());
             } else {
                 panic!("command expects a string");
             }
-        } else {
-            panic!("The command attribute is of the form #[command = \"name\"]");
         }
-    };
+        Meta::List(list) => {
+            for expr in list
+                .parse_args_with(Punctuated::<ExprAssign, Token![,]>::parse_terminated)
+                .unwrap()
+            {
+                match parse_expr_to_ident(&expr.left).to_string().as_str() {
+                    "prefix" => {
+                        if prefix.is_some() {
+                            panic!("Duplicate prefix");
+                        }
 
-    None
+                        prefix = match &*expr.right {
+                            Expr::Lit(ExprLit {
+                                attrs: _,
+                                lit: Lit::Str(s),
+                            }) => Some(s.value()),
+                            _ => panic!("Prefix should be a string"),
+                        }
+                    }
+                    "arg_type" => {
+                        if argument_type.is_some() {
+                            panic!("Duplicate arg_type");
+                        }
+
+                        argument_type = Some(parse_expr_to_ident(&expr.right).clone());
+                    }
+                    _ => {
+                        panic!("Unknown option")
+                    }
+                }
+            }
+        }
+        _ => {
+            panic!("command supports only a string value or a list of assignmets")
+        }
+    }
+
+    (prefix, argument_type)
 }
 
 #[proc_macro_derive(HyprlandDataWithArgument, attributes(command))]
@@ -38,13 +85,30 @@ pub fn hyprland_data_with_argument_derive(input: TokenStream) -> TokenStream {
     let derive = parse_macro_input!(input as DeriveInput);
     let name = &derive.ident;
 
-    let name_str = match parse_command(&derive) {
+    let mut name_str = None;
+    let mut arg_type = None;
+
+    for attr in derive.attrs {
+        (name_str, arg_type) = parse_command(&attr);
+        if name_str.is_some() || arg_type.is_some() {
+            break;
+        }
+    }
+
+    let name_str = match name_str {
         Some(name) => name,
         None => name.to_string().to_lowercase(),
     };
 
+    let arg_type = match arg_type {
+        Some(arg_type) => arg_type,
+        None => Ident::new("String", Span::call_site()),
+    };
+
     quote! {
         impl HyprlandDataWithArgument for #name {
+            type Argument = #arg_type;
+
             fn get_command(arg: String) -> String {
                 return format!("{} {}", #name_str, arg).to_owned();
             }
@@ -58,7 +122,15 @@ pub fn hyprland_data_derive(input: TokenStream) -> TokenStream {
     let derive = parse_macro_input!(input as DeriveInput);
     let name = &derive.ident;
 
-    let name_str = match parse_command(&derive) {
+    let mut name_str = None;
+    for attr in derive.attrs {
+        (name_str, _) = parse_command(&attr);
+        if name_str.is_some() {
+            break;
+        }
+    }
+
+    let name_str = match name_str {
         Some(name) => name,
         None => name.to_string().to_lowercase(),
     };
